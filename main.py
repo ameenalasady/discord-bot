d0d58b33f9e4168b77ccc86766df76b5f4945cea
ameenalasady <105679326+ameenalasady@users.noreply.github.com>
2023-05-09 01:46:04 -0400

    Railway.app hosting

--- main.py
+++ main.py
@@ -1,6 +1,52 @@
-import bot
+import discord
+import responses
+import logging
+import os
 
-if __name__ == '__main__':
-    bot.run_discord_bot()
+logging.basicConfig(level=logging.ERROR)
 
-    
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        async with message.author.typing():
+            await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except discord.Forbidden:
+        logging.error(
+            "Bot doesn't have permission to send a message to the user")
+    except discord.HTTPException:
+        logging.error("Failed to send message due to an HTTP error")
+
+
+def run_discord_bot():
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        logging.info(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        logging.info(f'{username} said: "{user_message}" ({channel})')
+
+        if user_message.startswith('?'):
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(str(os.getenv('DISCORD_TOKEN')))
+
+
+run_discord_bot()
--- responses.py
+++ responses.py
@@ -1,54 +1,47 @@
 import discord
 import wolframalpha
-import creds
 import requests
 from bs4 import BeautifulSoup
+import os
 
-help = 'To ask a question: q "question"\n To check the price of a stock: price "stock symbol"'
+# Rename 'help' variable to 'help_message' to avoid conflicts with built-in keyword
+help_message = 'To ask a question: q "question"\n To check the price of a stock: price "stock symbol"'
 
+# Use 'startswith()' method instead of converting message to a list and accessing first character
 def get_response(message: str) -> str:
-
     p_message = message.lower()
-    l_msg = p_message.split()
-    
+
     if '1071561802808303666' in message:
-        for i in l_msg:
-            if i in ['hi', 'hey', 'sup', 'yo', 'hello', 'wsup', 'wassup']:
-                return i
-        return help
+        for greeting in ['hi', 'hey', 'sup', 'yo', 'hello', 'wsup', 'wassup']:
+            if greeting in p_message:
+                return greeting
+        return help_message
 
+    if message.startswith('q '):
+        question = message[2:]
+        client = wolframalpha.Client(str(os.getenv('WOLFRAM_TOKEN')))
+        res = client.query(question)
+        answer = next(res.results).text
+        return answer
 
-    if list(message)[0] == 'q':
-        if list(message)[1] == ' ':
-        
-            question = message[1:]
-            client = wolframalpha.Client(creds.app_id)
-            res = client.query(question)
-            answer = next(res.results).text
-            return answer
+    if message.startswith('price '):
+        message_price = message.split()
+        page = requests.get(f"https://www.marketwatch.com/investing/index/{message_price[1].lower()}")
+        soup = BeautifulSoup(page.text, "html.parser")
 
-    message_price = message.split()
-    page = requests.get("https://www.marketwatch.com/investing/index/" + message_price[1].lower())
-    soup = BeautifulSoup(page.text, "html.parser")
+        price = soup.findAll("span", attrs={"class": "value"})
+        price2 = soup.findAll("bg-quote", attrs={"class": "value"})
+        company = soup.findAll("h1", attrs={"class": "company__name"})
+        country = soup.find("span", attrs={"class": "company__market"})
+        change = soup.find("bg-quote", attrs={"field": "change"})
 
-    price = soup.findAll("span", attrs={"class":"value"})
-    price2 = soup.findAll("bg-quote", attrs={"class":"value"})
-    company = soup.findAll("h1", attrs={"class":"company__name"})
-    country = soup.find("span", attrs={"class":"company__market"})
-    change = soup.find("bg-quote", attrs={"field":"change"})
-
-    if message_price[0] == 'price':
-        
-        if country.text[0:6] == 'Canada':
+        if country.text.startswith('Canada'):
             return f'The current stock price of {message_price[1].upper()} ({company[0].text}) is {price[0].text} CAD.'
+
         try:
             return f'The current stock price of {message_price[1].upper()} ({company[0].text}) is {price[0].text} USD.'
         except:
             return f'The current stock price of {message_price[1].upper()} ({company[0].text}) is {price2[0].text} USD.'
 
-
-        
-
-            
-    
-
+        # Add error handling for invalid or not found stock symbols
+        # return 'Invalid stock symbol' or 'Stock symbol not found' as appropriate
